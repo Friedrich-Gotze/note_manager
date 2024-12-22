6fdae4cf815022dcce685b1a168d0ed3237b1cf7
Friedrich-Gotze <kuznetsovnikita9012@gmail.com>
2024-12-22 19:03:55 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+
+username = input("Имя пользователя:")
+title = input("Заголовок:")
+content = input("Описание:")
+status = input("Статус:")
+created_date = input("Дата создания в формате дд-мм-гг, например 10-11-2024:")
+issue_date = input("Дата истечения в формате дд-мм-гг, например 10-11-2024:")
+
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("Дата истечения:", issue_date)
